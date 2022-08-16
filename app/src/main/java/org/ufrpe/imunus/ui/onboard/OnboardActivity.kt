830d7bea3ec416e4ab5e2c862e37470a75104ac5
nekojess1 <al.souza.jessica@gmail.com>
2022-08-16 19:45:01 -0300

    feat: create OnboardActivity

---
+++ app/src/main/java/org/ufrpe/imunus/ui/onboard/OnboardActivity.kt
@@ -0,0 +1,12 @@
+package org.ufrpe.imunus.ui.onboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.ufrpe.imunus.R
+
+class OnboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboard)
+    }
+}
